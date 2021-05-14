11164f8c3d34d8bd98dc32431d4cc2802dc31501
leondolecinski <71120201+leondolecinski@users.noreply.github.com>
2021-05-14 16:16:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const container = document.createElement('div')
+container.setAttribute('class','grid')
+xd.appendChild(container)
+
+const nazwa = document.createElement('div')
+const nazwaaa = document.createElement('h2')
+const nazwaa = document.createElement('p')
+const btn = document.createElement('button')
+const btn1 = document.createElement('button1')
+nazwaaa.innerText="ksiazka1"
+nazwaa.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolores sint, eius voluptatum eaque recusandae illum hic vero et perferendis, quas obcaecati facilis iste! Ad odit mollitia corrupti in error, rem omnis deleniti vitae delectus laudantium obcaecati ullam! Maxime omnis beatae veniam laudantium unde vitae placeat asperiores at! Eveniet, adipisci!"
+nazwa.setAttribute('class','grid2')
+nazwa.appendChild(nazwaaa)
+nazwa.appendChild(nazwaa)
+container.appendChild(nazwa)
+
+
+const n12 = document.createElement('div')
+const n2 = document.createElement('h2')
+const n1 = document.createElement('p')
+const btn2 = document.createElement('button')
+const btn3 = document.createElement('button1')
+n2.innerText="ksiazka1"
+n1.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolores sint, eius voluptatum eaque recusandae illum hic vero et perferendis, quas obcaecati facilis iste! Ad odit mollitia corrupti in error, rem omnis deleniti vitae delectus laudantium obcaecati ullam! Maxime omnis beatae veniam laudantium unde vitae placeat asperiores at! Eveniet, adipisci!"
+n12.setAttribute('class','grid2')
+n12.appendChild(n2)
+n12.appendChild(n1)
+container.appendChild(n12)
+
+const nazwa111 = document.createElement('div')
+const nazwa1 = document.createElement('h2')
+const nazwa11 = document.createElement('p')
+const btn4 = document.createElement('button')
+const btn5 = document.createElement('button1')
+nazwa1.innerText="ksiazka1"
+nazwa11.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolores sint, eius voluptatum eaque recusandae illum hic vero et perferendis, quas obcaecati facilis iste! Ad odit mollitia corrupti in error, rem omnis deleniti vitae delectus laudantium obcaecati ullam! Maxime omnis beatae veniam laudantium unde vitae placeat asperiores at! Eveniet, adipisci!"
+nazwa111.setAttribute('class','grid2')
+nazwa111.appendChild(nazwa1)
+nazwa111.appendChild(nazwa11)      
+container.appendChild(nazwa111)
